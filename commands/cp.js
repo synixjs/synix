dfc946d272af2c629639a93f49a053aed8b869b6
hemanth-hk <uppadahemanthkumar27@gmail.com>
2020-04-06 19:06:20 +0530

    Added links to cp docs

--- commands/cp.js
+++ commands/cp.js
@@ -7,7 +7,8 @@ function cp(old_path, new_path) {
 
     fs.copyFile(old_path_wrt, new_path_wrt, (err) => {
         if (err) {
-            console.log("File cannot be copied");
+            console.log("File cannot be copied please check the filenames and try again");
+            console.log('For `cp` docs visit https://synix-docs.netlify.com/ccc/commands.html#cp')
         } else {
             console.log(`File ${old_path} has been copied to ${new_path}`);
         }
